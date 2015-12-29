ab22b5b98e8fdd1096c9dead165f4134d40bc0da
Sandy2095 <santhosh.kumar56@wipro.com>
2015-12-29 10:38:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='Hi Sandy',
+      url='https://github.com/Sandy2095/Test_Package',
+      author='Sandy',
+      author_email='santhosh.kumar56@wipro.com',
+      license='NA',
+      packages=['Test_Package'],
+      zip_safe=False)
